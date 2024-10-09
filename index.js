407d55f9b2faed4ca04e66cb9efb047272457c76
Shuvadeep Chakraborty <shuvadeep.8719@gmail.com>
2024-10-09 07:22:06 +0530

    code files updated

---
+++ index.js
@@ -0,0 +1,21 @@
+//Filename- index.js
+
+//Importing express module
+const express= require("express");
+const app= express();
+
+//Handling GET / request
+app.use("/", (req, resp, next) => {
+    resp.send("This is the express server");
+
+});
+
+//Handling GET /hello request
+app.use("/hello", (req, resp, next) => {
+    resp.send("This is the hello response from server");
+    
+});
+
+app.listen(3000, () => {
+    console.log("Server in running");
+})
